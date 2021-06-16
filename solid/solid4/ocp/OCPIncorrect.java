9b33537e2b471dabe02cab10995378f8a2185cc6
vilasvarghese <vilas.varghese@gmail.com>
2021-06-17 04:52:29 +0530

    latest

---
+++ solid/solid4/ocp/OCPIncorrect.java
@@ -0,0 +1,10 @@
+public class AreaCalculator{
+	public double calculateArea(int side){
+		return side * side;
+	}
+	
+	public double calculateArea(int length, int breadth){
+		return length * breadth;
+	}
+	
+}
